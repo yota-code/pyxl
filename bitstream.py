0811424fb48ff25e69de3fd47e9461e07f7c0a79
yoochan <yota.news@gmail.com>
2023-03-14 20:57:09 +0100

    bitstream validated

--- bitstream.py
+++ bitstream.py
@@ -6,61 +6,99 @@ class BitStream() :
 		self.data = data
 		self.curs = 0
 
+		print(' - '.join(str(self[i]) for i in range(len(self))))
+
+	def seek(self, bitindex:int) :
+		self.curs = bitindex
+
+	def Un(self, n:int) :
+		if not 1 <= n :
+			raise ValueError
+		m = self[self.curs:self.curs+n]
+		self.curs += n
+		return m
+
+	def U32(self, * f_lst) :
+		return f_lst[self.read_Un(2)]()
+
+	def _reindex(self, i) :
+		if i < 0 :
+			i += len(self)
+		if not 0 <= i < len(self) :
+			raise IndexError(f"{i} not in [0; {len(self)}[")
+		return i
+
 	def __len__(self) :
 		return len(self.data) * 8
 	
-	def bit_address(self, k) :
-		return k // 8, 7 - k % 8
-
-	def _get_range(self, k_slice) :
-		o_start, b_start = self.bit_address(k_slice.start)
-		o_stop, b_stop = self.bit_address(k_slice.stop)
-
-		print("start:", k_slice.start, o_start, b_start)
-		print("stop:", k_slice.stop, o_stop, b_stop)
-
-		m_start = self.data[o_start] & ((1 << (b_start + 1)) - 1)
-
-		print(f"{((1 << (b_start + 1)) - 1):b} {m_start:b}")
-
-		if o_start == o_stop :
-			print("RAAAH", m_start >> b_stop)
-			return m_start >> b_stop
+	def read_bytes(self, n) :
 		
-		return "TUTU"
+	
+	def bit_address(self, k) :
+		return k // 8, k % 8
 
-		# return int.from_bytes(
-		# 	self.data[o_start] & 1 << ((b_start + 1) - 1) + 
-		# 	self.data[o_start:o_stop]
-		# ) >> (7 - b_stop)
+	def _get_bitslice(self, k_slice) :
+		s_start, s_stop = self._reindex(k_slice.start), self._reindex(k_slice.stop)
 
-	def _get_bit(self, k_index) :
+		o_start, b_start = self.bit_address(s_start)
+		o_stop, b_stop = self.bit_address(s_stop)
+
+		# print(f"start: {k_slice.start} :: {o_start}.{b_start}")
+		# print(f"stop: {k_slice.stop} :: {o_stop}.{b_stop}")
+
+		# print(f"self.data[o_start] = {hex(self.data[o_start])}")
+		# print(f"((1 << (7 - b_start + 1)) - 1) = {hex(((1 << (7 - b_start + 1)) - 1))}")
+		# print(f"self.data[o_start+1:o_stop+1] = {self.data[o_start+1:o_stop+2]}")
+		# print(f"""int.from_bytes(
+		# 	(self.data[o_start] & ((1 << (7 - b_start + 1)) - 1)).to_bytes(1, byteorder='big') + self.data[o_start+1:o_stop+2],
+		# 	byteorder='big'
+		# ) = {int.from_bytes(
+		# 	(self.data[o_start] & ((1 << (7 - b_start + 1)) - 1)).to_bytes(1, byteorder='big') + self.data[o_start+1:o_stop+1],
+		# 	byteorder='big'
+		# )}""")
+
+		return int.from_bytes(
+			(self.data[o_start] & ((1 << (7 - b_start + 1)) - 1)).to_bytes(1, byteorder='big') + self.data[o_start+1:o_stop+1],
+			byteorder='big'
+		) >> 8 - b_stop
+
+	def _get_bitindex(self, k_index) :
 		o, b = self.bit_address(k_index)
-		return 1 if (self.data[o] & 0x1 << (b)) else 0
+		return 1 if (self.data[o] & 0x1 << (7 - b)) else 0
 
 	def __getitem__(self, key) :
 		if isinstance(key, slice) :
 			# return [self[ii] for ii in xrange(*key.indices(len(self)))]
-			return self._get_range(key)
+			return self._get_bitslice(key)
 		elif isinstance(key, int) :
-			if key < 0 : # Handle negative indices
+			if key < 0 :
 				key += len(self)
 			if key < 0 or key >= len(self):
 				raise IndexError(f"The index {key} is out of range.")
-			return self._get_bit(key) # Get the data from elsewhere
+			return self._get_bitindex(key)
 		else:
 			raise TypeError("Invalid argument type.")
     
-	def read_U32(self, ) :
-		pass
 
 
 if __name__ == '__main__' :
-	c = b'\x80\x40\xFF\xC5'
-	u = BitStream(c)
 
-	for i in range(len(u)) :
-		print(u[i], end=' - ')
-	print()
+	print(bin(BitStream(b'\x00\x71\xC0')[9:19]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:18]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:17]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:16]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:15]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:14]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:13]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:12]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:11]))
+	print(bin(BitStream(b'\x00\x71\xC0')[9:10]))
 
-	print(f"{u[8:12]:b}")
+	# c = b'\x80\x40\xFF\xC5'
+	# u = BitStream(c)
+
+	# for i in range(len(u)) :
+	# 	print(u[i], end=' - ')
+	# print()
+
+	# print(f"{u[8:12]:b}")
